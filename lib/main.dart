d571121fd16354f515d6eb3e89f3c5462f63068c
traleven <idipster@gmail.com>
2022-01-30 15:36:25 +0200

    Final strokes 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alchemy Calculator',
+      title: 'Алхимический калькулятор',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(
